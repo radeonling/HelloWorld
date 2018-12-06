d82d30414328c455318c10fd3eca6715a769d2c3
radeonling <radeon_ling@126.com>
2018-12-06 17:52:40 +0800

    no

---
+++ HelloWorld.py
@@ -0,0 +1,15 @@
+print("Hello world")
+
+age = int(input('Input your age: '))
+if age >= 18:
+    print('You are an adult.')
+elif age >= 6:
+    print('You are a teenager.')
+else:
+    print('You are a kid.')
+
+s = 'Python-中文'
+# print(s)
+# b = s.encode('utf-8')
+# print(b)
+# print(b.decode('utf-8'))
